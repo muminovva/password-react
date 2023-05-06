07fbe550db8e998b01003966538f909156aa8323
muminovva <muminovvaumida@gmail.com>
2023-05-06 08:24:16 +0500

    eye

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function HideAndShowPassword(){
+    const [show,setShow]=useState(false)
+    const handleShow=()=>{
+        setShow(!show)
+    }
+    return(
+        <div className="container">
+            <input type={show?"text":"password"} />
+            <label onClick={handleShow}>{show?<i class="fa-regular fa-eye"></i>:<i class="fa-regular fa-eye-slash"></i>}</label>
+        </div>
+    );
 }
-
-export default App;
+export default HideAndShowPassword;
